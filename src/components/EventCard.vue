49cbc5f1f1116f9f1923708d60232f8f28ceee00
yuriikhomych <yuriykhomich@gmail.com>
2020-09-26 21:41:34 +0300

    First steps in vue js router

---
+++ src/components/EventCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 v-if="" @click="handler(arg, event)"></h1>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import New from '@/components/New.vue';
+export default {
+    props: {
+        propName: {
+            type: Number,
+            default: 
+        },
+    },
+  data() {
+      return {
+          key: value
+      }
+  },
+}
+</script>
+
+<style></style>
