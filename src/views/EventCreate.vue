9a3a1c959d1613fbd525a7f39e0b0b42d7367264
yuriikhomych <yuriykhomich@gmail.com>
2020-09-30 21:08:20 +0300

    Use vuex modules

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -78,7 +78,7 @@ export default {
   methods: {
     createEvent() {
       this.$store
-        .dispatch('createEvent', this.event)
+        .dispatch('event/createEvent', this.event)
         .then(() => {
           this.$router.push({
             name: 'event-show',
@@ -91,7 +91,7 @@ export default {
         })
     },
     createFreshEventObject() {
-      const user = this.$store.state.user
+      const user = this.$store.state.user.user
       const id = Math.floor(Math.random() * 10000000)
 
       return {
