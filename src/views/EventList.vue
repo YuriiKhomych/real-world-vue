860e04a1162dd68ba8bf24ce1744d196825556ab
yuriikhomych <yuriykhomich@gmail.com>
2020-10-03 21:56:21 +0300

    Add basic progress bar to api calls via axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -6,17 +6,18 @@
       <router-link
         :to="{ name: 'event-list', query: { page: page - 1 } }"
         rel="prev"
-        >Prev Page</router-link
+      >
+        Prev Page</router-link
       >
       <template v-if="hasNextPage"> | </template>
     </template>
-    <template v-if="hasNextPage">
-      <router-link
-        :to="{ name: 'event-list', query: { page: page + 1 } }"
-        rel="next"
-        >Next Page</router-link
-      >
-    </template>
+    <router-link
+      v-if="hasNextPage"
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+    >
+      Next Page</router-link
+    >
   </div>
 </template>
 
@@ -31,6 +32,7 @@ export default {
   created() {
     this.perPage = 3 // Setting perPage here and not in data means it won't be reactive.
     // We don't need it to be reactive, and this way our component has access to it.
+
     this.$store.dispatch('event/fetchEvents', {
       perPage: this.perPage,
       page: this.page
@@ -41,7 +43,7 @@ export default {
       return parseInt(this.$route.query.page) || 1
     },
     hasNextPage() {
-      return this.event.totalEvents > this.page * this.perPage
+      return this.event.eventsTotal > this.page * this.perPage
     },
     ...mapState(['event', 'user'])
   }
