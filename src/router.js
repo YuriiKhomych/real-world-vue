72b7c37cc9693ff99ed4728633a3952ef0c2ceea
yuriikhomych <yuriykhomich@gmail.com>
2020-10-04 18:22:08 +0300

    Add example with basic form validation

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import NotFound from './views/NotFound.vue'
 import NetworkIssue from './views/NetworkIssue.vue'
 import NProgress from 'nprogress'
 import store from '@/store/store'
+import Example from './views/Example.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,10 @@ const routes = [
     name: 'event-list',
     component: EventList,
     props: true
+  },
+  {
+    path: '/example',
+    component: Example
   },
   {
     path: '/event/:id',
